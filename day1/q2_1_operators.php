0e5a0772b3fce29e55ec8f5cdd5ab5ea03d9ed6d
SiddhiN210 <siddhinair02@gmail.com>
2020-10-02 22:47:38 +0530

    assignments day1&2

---
+++ day1/q2_1_operators.php
@@ -0,0 +1,12 @@
+<?php
+$n1=10;
+$n2=20;
+$add=$n1+$n2 ;
+echo " The addition of two numbers is $add ";
+$sub=$n1-$n2 ;
+echo " The subtraction of two numbers is $sub ";
+$mul=$n1*$n2 ;
+echo " The sum of two numbers is $mul ";
+$div=$n1/$n2 ;
+echo " The sum of two numbers is $div ";
+?>
---
+++ day1/q2_2_operators.php
@@ -0,0 +1,9 @@
+<?php
+$num1=30;
+$num2=15;
+if($num1>$num2){
+	echo "$num1 is greater";
+}
+else
+	echo"$num2 is greater";
+?>
